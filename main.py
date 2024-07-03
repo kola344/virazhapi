368d3ed6ebec73d34b68c159665542659e573de2
kola <koli>
2024-07-03 20:29:15 +0300

    a

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from aiogram.types import Update
 from virazh_bot import bot_init
 import config
 from virazh_bot.admin.admin_messages import router as admin_router
+from aiogram import Bot, Dispatcher
+from typing import Any
 
 app = FastAPI()
 
@@ -17,13 +19,8 @@ async def index_page():
         return {"Status": False, "init": f"err: {e}"}
 
 @app.post('/bot_hook')
-async def webhook(request: Update):
-    json_str = request.json()
-    update = Update.to_object(json_str)
-    await bot_init.dp.feed_update(bot_init.bot, update)
-    # update = Update(**await request.json())
-    # await bot_init.dp.feed_update(bot_init.bot, update)
-    return {'status': 'ok'}
+async def webhook(update: dict[str, Any]) -> None:
+    await bot_init.dp.feed_webhook_update(bot=bot_init.bot, update=Update(**update))
 
 @app.on_event('startup')
 async def on_startup():
