67bcadb0c8e326bc8e63b9a1889b8f22e383808c
kola <koli>
2024-07-07 17:14:14 +0300

    beta 1

--- virazh_bot/manager/replics.py
+++ virazh_bot/manager/replics.py
@@ -49,20 +49,20 @@ async def replic_order_manager_markup(selected_status, order_id):
         return keyboards.order_cancelled, keyboards.order_cancelled
     keyboard = []
     # info_buttons
-    keyboard.append([InlineKeyboardButton(text='Статусы', callback_data=f'order')])
+    keyboard.append([InlineKeyboardButton(text='Статусы', callback_data=f'no_using')])
     # statuses
     for i in config.order_statuses:
         if i == selected_status:
             keyboard.append([InlineKeyboardButton(text=f'➡️ {config.order_statuses[i]}', callback_data=f'order.set:{order_id}.cancelled')])
         else:
             keyboard.append([InlineKeyboardButton(text=config.order_statuses[i], callback_data=f'order.set:{order_id}.completed')])
-    keyboard.append([InlineKeyboardButton(text='Действия', callback_data=f'order')])
+    keyboard.append([InlineKeyboardButton(text='Действия', callback_data=f'no_using')])
     keyboard.append([InlineKeyboardButton(text='❌ Отменить заказ', callback_data=f'order.cancel.{order_id}')])
     keyboard.append([InlineKeyboardButton(text='✔️ Заказ выполнен', callback_data=f'order.complete.{order_id}')])
     if await db.orders.get_user_message_id(order_id) != None:
-        keyboard.append([InlineKeyboardButton(text='🟢 Telegram', callback_data=f'order')])
+        keyboard.append([InlineKeyboardButton(text='🟢 Telegram', callback_data=f'no_using')])
     else:
-        keyboard.append([InlineKeyboardButton(text='🔴 Telegram', callback_data=f'order')])
+        keyboard.append([InlineKeyboardButton(text='🔴 Telegram', callback_data=f'no_using')])
 
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     user_markup = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text=config.order_statuses[selected_status], callback_data=f'orderr')]])
