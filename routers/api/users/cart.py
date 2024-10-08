06cfe53848b47ef9457164f2924dc52fdf56c540
kola <koli>
2024-10-08 16:45:52 +0300

    added checking available items in user cart function

--- routers/api/users/cart.py
+++ routers/api/users/cart.py
@@ -14,7 +14,7 @@ async def add_to_cartPage(item: add_to_cartModel):
         cart_data.carts[item.user_key] = []
     item_data = await db.menu.get_item_info_by_id(item.item_id)
     cart_data.carts[item.user_key].append({"item_id": item.item_id, "variation_id": item.variation_id, "variation": item_data["variations"][item.variation_id], "price": item_data["price"][item.variation_id], "name": item_data["name"], "info": item_data["info"], "subinfo": item_data["subinfo"]})
-    return {"status": True, "info": "success", "cart": cart_data.carts[item.user_key]}
+    return {"status": True, "info": "success"}
 
 
 @router.post('/delete_from_cart')
@@ -22,7 +22,7 @@ async def delete_from_cartPage(item: delete_from_cartModel):
     # cart = cart_data.carts[item.user_key]
     # filtered_cart = [d for d in cart if d["item"] != item.item_id]
     cart_data.carts[item.user_key].pop(item.item_index)
-    return {"status": True, "info": "success", "cart": cart_data.carts[item.user_key]}
+    return {"status": True, "info": "success"}
 
 
 @router.post('/get_cart')
