3e189c994fd7c37af70c3ef5b557146b3c5e2360
kola <koli>
2024-10-14 19:37:03 +0300

    added preorder

--- routers/api/users/orders.py
+++ routers/api/users/orders.py
@@ -88,27 +88,25 @@ async def send_order_daysPage(item: send_order_daysModel):
     order_type = 'pickup'
     if item.address != 'Самовывоз':
         order_type = 'delivery'
-    if item.delivery_at in get_available_times(order_type):
-        await db.users.update_name_by_key(item.user_key, item.name)
-        phone_number = await db.users.get_phone_by_key(item.user_key)
-        order_subtext = ''
-        price = 0
-        for i in carts[item.user_key]:
-            try:
-                price += int(i["price"])
-            except:
-                price += 0
-            order_subtext += f'\n{i["name"]} - {i["variation"]}: {i["price"]}'
-        current_date = datetime.now()
-        date = current_date.strftime('%d.%m.%Y')
-        order_id = await db.orders.add_order(carts[item.user_key], item.delivery_at, item.comment, item.user_key, item.address, date, price, item.payment)
-        if price >= gift_target:
-            gift_data = await db.text_table.get_gift()
-            text = f'ЗАКАЗ #{order_id}{order_subtext}\n🎁{gift_data["name"]}: 0\nИТОГО: {price}\n\nИмя: {item.name}\nАдрес доставки: {item.address}\nДоставить к: {item.date} {item.delivery_at}\n\nКомментарий к заказу:\n{item.comment}\nОплата: {item.payment}\nЭто предказаз. Доставить {item.date}\nНомер: {phone_number}'
-        else:
-            text = f'ЗАКАЗ #{order_id}{order_subtext}\nИТОГО: {price}\n\nИмя: {item.name}\nАдрес доставки: {item.address}\nДоставить к: {item.delivery_at}\n\nКомментарий к заказу:\n{item.comment}\nОплата: {item.payment}\nЭто предказаз. Доставить {item.date}\nНомер: {phone_number}'
-        await db.orders.update_text(order_id, text)
-        await orders_bot.send_order_to_chat(text, item.user_key, order_id)
-        carts[item.user_key] = []
-        return {"status": True, "info": "success", "order_id": order_id}
-    return {"status": False, "info": 'time is not available', "order_id": ''}
+    await db.users.update_name_by_key(item.user_key, item.name)
+    phone_number = await db.users.get_phone_by_key(item.user_key)
+    order_subtext = ''
+    price = 0
+    for i in carts[item.user_key]:
+        try:
+            price += int(i["price"])
+        except:
+            price += 0
+        order_subtext += f'\n{i["name"]} - {i["variation"]}: {i["price"]}'
+    current_date = datetime.now()
+    date = current_date.strftime('%d.%m.%Y')
+    order_id = await db.orders.add_order(carts[item.user_key], item.delivery_at, item.comment, item.user_key, item.address, date, price, item.payment)
+    if price >= gift_target:
+        gift_data = await db.text_table.get_gift()
+        text = f'ЗАКАЗ #{order_id}{order_subtext}\n🎁{gift_data["name"]}: 0\nИТОГО: {price}\n\nИмя: {item.name}\nАдрес доставки: {item.address}\nДоставить к: {item.date} {item.delivery_at}\n\nКомментарий к заказу:\n{item.comment}\nОплата: {item.payment}\nЭто предказаз. Доставить {item.date}\nНомер: {phone_number}'
+    else:
+        text = f'ЗАКАЗ #{order_id}{order_subtext}\nИТОГО: {price}\n\nИмя: {item.name}\nАдрес доставки: {item.address}\nДоставить к: {item.delivery_at}\n\nКомментарий к заказу:\n{item.comment}\nОплата: {item.payment}\nЭто предказаз. Доставить {item.date}\nНомер: {phone_number}'
+    await db.orders.update_text(order_id, text)
+    await orders_bot.send_order_to_chat(text, item.user_key, order_id)
+    carts[item.user_key] = []
+    return {"status": True, "info": "success", "order_id": order_id}
